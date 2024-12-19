63e04292938fda1969537a63ac233af6be003c68
rizzolib <jamesybarra98114@gmail.com>
2024-12-19 10:35:06 -0700

    solana telegram bot for tracking wallet 2 main wallets
    solana telegram bot for tracking wallet 2 main wallets

--- src/config/config.ts
+++ src/config/config.ts
@@ -3,12 +3,13 @@ dotenv.config();
 
 export const PUMP_FUN_ADDRESS = "6EF8rrecthR5Dkzon8Nwu78hRvfCKubJ14M5uBEwF6P";
 
-export const MAIN_WALLET_ADDRESS = process.env.MAIN_WALLET_ADDRESS || "";
+export const MAIN_WALLET_ADDRESS_1 = process.env.MAIN_WALLET_ADDRESS_1 || "";
+export const MAIN_WALLET_ADDRESS_2 = process.env.MAIN_WALLET_ADDRESS_2 || "";
 export const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN || "";
 export const TELEGRAM_CHANNEL_ID = process.env.TELEGRAM_CHANNEL_ID || "";
 export const SOLANA_RPC_URL = process.env.SOLANA_RPC_URL || "";
 
-export const TRACKED_WALLETS_SIZE = 100;
+export const TRACKED_WALLETS_SIZE = 1000;
 
 // log file
 export const LOGFILE = "wallet_tracker.log";
