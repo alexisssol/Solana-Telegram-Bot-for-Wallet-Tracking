0551259029f54fde114ff46e724a61dd7024dbf8
rizzolib <jamesybarra98114@gmail.com>
2024-12-23 12:18:40 -0700

    solana telegram bot for tracking wallet fixing small wallet bugs
    solana telegram bot for tracking wallet fixing small wallet bugs

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -7,7 +7,7 @@ import {
   LOGFILE,
   MAIN_WALLET_ADDRESS_1,
   MAIN_WALLET_ADDRESS_2,
-  PUMP_FUN_ADDRESS,
+  MAX_BALANCE_CHANGE,
   SOLANA_RPC_URL,
   TELEGRAM_BOT_TOKEN,
   TELEGRAM_CHANNEL_ID,
@@ -47,19 +47,8 @@ export class WalletTracker {
     this.trackedWallets_2 = new Map();
 
     this.pumpfunTokens = new Map();
-    // this.initDatabase();
-    // this.loadTrackedWallets(1);
-    // this.loadTrackedWallets(2);
   }
 
-  // private initDatabase(): void {
-  //   this.db.run(`
-  //           CREATE TABLE IF NOT EXISTS tracked_wallets (
-  //               address TEXT PRIMARY KEY,
-  //               timestamp INTEGER
-  //           )
-  //       `);
-  // }
   public saveLog(message: string): void {
     const logFile = LOGFILE;
     const maxSize = LOG_MAX_SIZE;
@@ -71,36 +60,17 @@ export class WalletTracker {
       }
     }
 
-    const timestamp = new Date().toISOString();
-    const logMessage = `[${timestamp}] ${message}\n`;
+    // const timestamp = new Date().toISOString();
+    // const logMessage = `[${timestamp}] ${message}\n`;
+    const logMessage = `${message}\n`;
     fs.appendFileSync(logFile, logMessage);
   }
-
-  // private loadTrackedWallets(wallet_id: number): void {
-  //   this.db.all("SELECT * FROM tracked_wallets", (err, rows: WalletTrack[]) => {
-  //     if (err) {
-  //       console.error("Error loading wallets:", err);
-  //       return;
-  //     }
-  //     rows.forEach((row) => {
-  //       if (wallet_id === 1) {
-  //         this.trackedWallets_1.set(row.address, row);
-  //       } else {
-  //         this.trackedWallets_2.set(row.address, row);
-  //       }
-  //     });
-  //   });
-  // }
 
   private async trackNewWallet(
     wallet_id: number,
     address: string
   ): Promise<void> {
     const timestamp = Date.now();
-    // this.db.run(
-    //   "INSERT INTO tracked_wallets (address, timestamp) VALUES (?, ?)",
-    //   [address, timestamp]
-    // );
     if (wallet_id === 1)
       this.trackedWallets_1.set(address, { address, timestamp });
     if (wallet_id === 2)
@@ -111,10 +81,6 @@ export class WalletTracker {
     address: string
   ): Promise<void> {
     const timestamp = Date.now();
-    // this.db.run("UPDATE tracked_wallets SET timestamp = ? WHERE address = ?", [
-    //   timestamp,
-    //   address,
-    // ]);
     if (wallet_id === 1)
       this.trackedWallets_1.set(address, {
         address: address,
@@ -128,6 +94,62 @@ export class WalletTracker {
       });
   }
 
+  private async MonitorSmallWallets(
+    id: number,
+    newTrackedWalletAddress: string
+  ) {
+    // monitor small wallet from main wallet 1
+    this.connection.onLogs(
+      new PublicKey(newTrackedWalletAddress),
+      async ({ logs, err, signature }) => {
+        try {
+          if (err) return;
+          // console.log(`${newTrackedWalletAddress} Logs:`);
+          // this.saveLog(`${newTrackedWalletAddress} Logs: ${logs}`);
+          const CA = await getSignature2CA(this.connection, signature);
+          // console.log("CA:", CA);
+
+          if (CA) {
+            this.saveLog(
+              `small wallet tx: sm: ${newTrackedWalletAddress}, token: ${CA}, siggnature: ${signature}`
+            );
+            const CA_ADDRESS = CA.toString();
+            const tmpAnother = 3 - id;
+            if (
+              CA_ADDRESS === MAIN_WALLET_ADDRESS_1 ||
+              CA_ADDRESS === MAIN_WALLET_ADDRESS_2
+            ) {
+              this.pumpfunTokens.set(CA_ADDRESS, -1);
+            } else if (
+              this.pumpfunTokens.get(CA_ADDRESS) === tmpAnother ||
+              this.pumpfunTokens.get(CA_ADDRESS) === 3
+            ) {
+              const { symbol, mc } = await getTokenInfo(
+                this.connection,
+                CA_ADDRESS
+              );
+              await this.sendTelegramNotification(
+                symbol,
+                mc,
+                newTrackedWalletAddress,
+                CA_ADDRESS,
+                signature
+              );
+              this.saveLog(
+                `TG alert: 📝 ${id} Main wall: sm:${newTrackedWalletAddress} => ca:${CA_ADDRESS}, tx: ${signature}`
+              );
+              this.pumpfunTokens.set(CA_ADDRESS, 3);
+            } else if (this.pumpfunTokens.get(CA_ADDRESS) !== -1) {
+              this.pumpfunTokens.set(CA_ADDRESS, id);
+            }
+          }
+        } catch (error) {
+          this.saveLog(`Error: ${error}`);
+        }
+      }
+    );
+  }
+
   private async sendTelegramNotification(
     symbol: string,
     mc: string,
@@ -135,13 +157,6 @@ export class WalletTracker {
     ca: string,
     signature: string
   ): Promise<void> {
-    // const message = `🔗 ${shortenAddressWithLink(
-    //   walletAddress
-    // )} has interacted with pump.fun. | ${txnLink(
-    //   signature
-    // )} | ${shortenAddressWithLink(ca)}
-    // `;
-
     const message = `🔗 ${shortenAddressWithLink(
       ca,
       symbol
@@ -162,188 +177,87 @@ export class WalletTracker {
     }
   }
 
-  public async monitorTransactions(): Promise<void> {
+  public async monitorTransactions(id: number): Promise<void> {
     // Monitor main wallet 1
+    const MAIN_WALLET_ADDRESS =
+      id === 1 ? MAIN_WALLET_ADDRESS_1 : MAIN_WALLET_ADDRESS_2;
     try {
       this.connection.onLogs(
-        new PublicKey(MAIN_WALLET_ADDRESS_1),
+        new PublicKey(MAIN_WALLET_ADDRESS),
         async ({ logs, err, signature }) => {
-          if (err) return;
+          try {
+            if (err) return;
 
-          const data = await getTransactionDetails(this.connection, signature);
-          console.log("1 Data:", data?.signature);
-          this.saveLog(`Main wallet txn: ${data?.signature}`);
-
-          if (data?.balanceChange) {
-            const balanceValue = parseFloat(
-              data.balanceChange.replace(" SOL", "")
+            const data = await getTransactionDetails(
+              this.connection,
+              signature
             );
-            if (Math.abs(balanceValue) < 25) {
-              for (const instruction of data?.instructions) {
-                const newTrackedWalletAddress = instruction.receiver;
-                if (
-                  instruction.program === "system" &&
-                  instruction.type === "transfer" &&
-                  newTrackedWalletAddress
-                ) {
-                  if (this.trackedWallets_1.has(newTrackedWalletAddress)) {
-                    await this.trackUpdateWallet(1, newTrackedWalletAddress);
-                    continue;
-                  }
-                  if (this.trackedWallets_1.size >= TRACKED_WALLETS_SIZE) {
-                    console.log(
-                      "Main wallet 1 tracked limited wallets. Skipping..."
-                    );
-                    continue;
-                  }
-                  await this.trackNewWallet(1, newTrackedWalletAddress);
+            console.log(`${id} Data:`, data?.signature);
+            let smWallets = [];
 
-                  // monitor small wallet from main wallet 1
-                  this.connection.onLogs(
-                    new PublicKey(newTrackedWalletAddress),
-                    async ({ logs, err, signature }) => {
-                      if (err) return;
-                      console.log(`${newTrackedWalletAddress} Logs:`);
-                      this.saveLog(`${newTrackedWalletAddress} Logs: ${logs}`);
-                      const CA = await getSignature2CA(
-                        this.connection,
-                        signature
-                      );
-                      console.log("CA:", CA);
-                      this.saveLog(`CA: ${CA}`);
+            if (data?.balanceChange && data.sender === MAIN_WALLET_ADDRESS) {
+              const balanceValue = parseFloat(
+                data.balanceChange.replace(" SOL", "")
+              );
+              if (Math.abs(balanceValue) < MAX_BALANCE_CHANGE) {
+                for (const instruction of data?.instructions) {
+                  const newTrackedWalletAddress = instruction.receiver;
 
-                      if (CA) {
-                        const CA_ADDRESS = CA.toString();
-                        if (
-                          CA_ADDRESS === MAIN_WALLET_ADDRESS_1 ||
-                          CA_ADDRESS === MAIN_WALLET_ADDRESS_2
-                        ) {
-                          this.pumpfunTokens.set(CA_ADDRESS, -1);
-                        } else if (
-                          this.pumpfunTokens.get(CA_ADDRESS) === 2 ||
-                          this.pumpfunTokens.get(CA_ADDRESS) === 3
-                        ) {
-                          const { symbol, mc } = await getTokenInfo(
-                            this.connection,
-                            CA_ADDRESS
-                          );
-                          await this.sendTelegramNotification(
-                            symbol,
-                            mc,
-                            newTrackedWalletAddress,
-                            CA_ADDRESS,
-                            signature
-                          );
-                          this.pumpfunTokens.set(CA_ADDRESS, 3);
-                        } else if (this.pumpfunTokens.get(CA_ADDRESS) !== -1) {
-                          this.pumpfunTokens.set(CA_ADDRESS, 1);
-                        }
-                      }
+                  if (
+                    instruction.program === "system" &&
+                    instruction.type === "transfer" &&
+                    newTrackedWalletAddress
+                  ) {
+                    smWallets.push(newTrackedWalletAddress);
+                    const tmpTrackedWallets =
+                      id === 1 ? this.trackedWallets_1 : this.trackedWallets_2;
+                    if (tmpTrackedWallets.has(newTrackedWalletAddress)) {
+                      await this.trackUpdateWallet(id, newTrackedWalletAddress);
+                      continue;
                     }
-                  );
+                    if (tmpTrackedWallets.size >= TRACKED_WALLETS_SIZE) {
+                      this.saveLog(
+                        `Main wallet ${id} tracked limited wallets. Skipping...`
+                      );
+                      continue;
+                    }
+                    await this.trackNewWallet(id, newTrackedWalletAddress);
+                    try {
+                      await this.MonitorSmallWallets(
+                        id,
+                        newTrackedWalletAddress
+                      );
+                    } catch (error) {
+                      this.saveLog(
+                        `Error monitoring ${id} transactions: ${error}`
+                      );
+                    }
+                  }
                 }
               }
             }
+            if (smWallets.length > 0) {
+              this.saveLog(
+                `${id} Main wallet txn: ${
+                  data?.signature
+                } smWallets: ${smWallets.join(", ")}`
+              );
+            }
+          } catch (error) {
+            console.log(`Error processing ${id} transactions:`, error);
           }
         },
         "confirmed"
       );
     } catch (error) {
-      console.log("Error monitoring 1 transactions:", error);
-      this.saveLog("Error monitoring 1 transactions");
-    }
-
-    // Monitor main wallet 2
-    try {
-      this.connection.onLogs(
-        new PublicKey(MAIN_WALLET_ADDRESS_2),
-        async ({ logs, err, signature }) => {
-          if (err) return;
-
-          const data = await getTransactionDetails(this.connection, signature);
-          console.log("2 Data:", data?.signature);
-          this.saveLog(`Main wallet txn: ${data?.signature}`);
-
-          if (data?.balanceChange) {
-            const balanceValue = parseFloat(
-              data.balanceChange.replace(" SOL", "")
-            );
-            if (Math.abs(balanceValue) < 25) {
-              for (const instruction of data?.instructions) {
-                const newTrackedWalletAddress = instruction.receiver;
-                if (
-                  instruction.program === "system" &&
-                  instruction.type === "transfer" &&
-                  newTrackedWalletAddress
-                ) {
-                  if (this.trackedWallets_2.has(newTrackedWalletAddress)) {
-                    await this.trackUpdateWallet(2, newTrackedWalletAddress);
-                    continue;
-                  }
-                  if (this.trackedWallets_2.size >= TRACKED_WALLETS_SIZE) {
-                    console.log(
-                      "Main wallet 2 tracked limited wallets. Skipping..."
-                    );
-                    continue;
-                  }
-                  await this.trackNewWallet(2, newTrackedWalletAddress);
-
-                  // monitor small wallet from main wallet 2
-                  this.connection.onLogs(
-                    new PublicKey(newTrackedWalletAddress),
-                    async ({ logs, err, signature }) => {
-                      if (err) return;
-                      console.log(`${newTrackedWalletAddress} Logs:`);
-                      this.saveLog(`${newTrackedWalletAddress} Logs: ${logs}`);
-                      const CA = await getSignature2CA(
-                        this.connection,
-                        signature
-                      );
-
-                      if (CA) {
-                        const CA_ADDRESS = CA.toString();
-                        if (
-                          CA_ADDRESS === MAIN_WALLET_ADDRESS_1 ||
-                          CA_ADDRESS === MAIN_WALLET_ADDRESS_2
-                        ) {
-                          this.pumpfunTokens.set(CA_ADDRESS, -1);
-                        } else if (
-                          this.pumpfunTokens.get(CA_ADDRESS) === 1 ||
-                          this.pumpfunTokens.get(CA_ADDRESS) === 3
-                        ) {
-                          const { symbol, mc } = await getTokenInfo(
-                            this.connection,
-                            CA_ADDRESS
-                          );
-                          await this.sendTelegramNotification(
-                            symbol,
-                            mc,
-                            newTrackedWalletAddress,
-                            CA_ADDRESS,
-                            signature
-                          );
-                          this.pumpfunTokens.set(CA_ADDRESS, 3);
-                        } else if (this.pumpfunTokens.get(CA_ADDRESS) !== -1) {
-                          this.pumpfunTokens.set(CA_ADDRESS, 2);
-                        }
-                      }
-                    }
-                  );
-                }
-              }
-            }
-          }
-        },
-        "confirmed"
-      );
-    } catch (error) {
-      console.log("Error monitoring 2 transactions:", error);
-      this.saveLog("Error monitoring 2 transactions");
+      console.log(`Error monitoring ${id} transactions:`, error);
+      this.saveLog(`Error monitoring ${id} transactions`);
     }
   }
 
   public async start(): Promise<void> {
-    await this.monitorTransactions();
+    await this.monitorTransactions(1);
+    await this.monitorTransactions(2);
     this.saveLog("Wallet tracker started...");
     console.log("Wallet tracker started...");
   }
